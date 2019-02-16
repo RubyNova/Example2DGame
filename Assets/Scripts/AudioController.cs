f58db161a8e2716f9d2634fae0a107aab831b31c
Matt Jones <matthewj866@gmail.com>
2019-02-16 20:26:29 +0100

    Added everything else.

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioController : MonoBehaviour
+{
+
+    public void ToggleMute(bool shouldMute)
+    {
+        AudioListener.volume = shouldMute ? 0 : 1;
+    }
+}
---
+++ Assets/Scripts/MainMenuUIController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuUIController : MonoBehaviour
+{
+    [SerializeField] private GameObject _mainMenu;
+    [SerializeField] private GameObject _optionsMenu;
+    [SerializeField] private AudioController _audioController;
+    
+    public void LogACall()
+    {
+        Debug.Log("Something called me!");
+    }
+
+    public void ShowOptions()
+    {
+        _mainMenu.SetActive(false);
+        _optionsMenu.SetActive(true);
+    }
+
+    public void ShowMainMenu()
+    {
+        _mainMenu.SetActive(true);
+        _optionsMenu.SetActive(false);
+    }
+
+    public void SetAudioState(Toggle state)
+    {
+        _audioController.ToggleMute(state.isOn);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
